1753010338b2d035b4678932e990b35ab5210caa
Suzan <suzanbulbl@gmail.com>
2022-11-16 16:59:00 +0300

    server.js add

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['+6FyICQFOZa8d66mdGQnnA==','KD/A1P7kPtPfgRuPHljdKA==','N6vRBNbgcrje1rqXNpLz4Q==','GInIf3PE3q8EFuEcKmdA+g=='],
+    // keys: env.array('APP_KEYS', ['+6FyICQFOZa8d66mdGQnnA==','KD/A1P7kPtPfgRuPHljdKA==','N6vRBNbgcrje1rqXNpLz4Q==','GInIf3PE3q8EFuEcKmdA+g==']),
   },
 });
