fb3e3e36a68128b524d0eaadedcfd03fd3de5462
Suzan <suzanbulbl@gmail.com>
2022-11-16 17:02:44 +0300

    some fix

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'a1a379bbcf4841b64fe051b00be2d36c'),
+  },
+  auth: {
+    secret: env('ADMIN_JWT_SECRET', '39c92c09b9be114b036a248331688d8c'),
   },
 });
